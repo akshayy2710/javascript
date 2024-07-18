7baad6578e7f95e0c928938956de1d48840db6b7
Akshay Donga <163103679+akshayy2710@users.noreply.github.com>
2024-07-18 18:12:15 +0530

    i2.js

--- pr-10/i2.js
+++ pr-10/i2.js
@@ -3,7 +3,7 @@ let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
 let cot = 0;
 for (let i = 0; i < str.length; i++) {
   if (vowels.includes(str[i])) {
-    count++;
+    cot++;
   }
 }
 console.log(cot);
