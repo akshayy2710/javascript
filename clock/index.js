441e008c82d584bff4d480f00fa9c537c5a5da5a
Akshay Donga <163103679+akshayy2710@users.noreply.github.com>
2024-10-24 16:39:35 +0530

    js

---
+++ clock/index.js
@@ -0,0 +1,36 @@
+const times = document.getElementById("time");
+const dates = document.getElementById("date");
+
+const clock = () => {
+  const date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+
+ if (minutes < 10) {
+  minutes = "0" + minutes;
+}
+
+if (seconds < 10) {
+  seconds = "0" + seconds;
+}
+
+if (hours > 12) {
+  hours -= 12;
+}
+
+if (hours < 10) {
+  hours = "0" + hours;
+}
+
+times.innerHTML = `${hours}:${minutes}:${seconds}`;
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const dayMonth = date.getDate();
+const year = date.getFullYear();
+const weekDay = days[date.getDay()];
+const month = months[date.getMonth()];
+dates.innerHTML = `${weekDay}, ${dayMonth} ${month} ${year}`;
+}
+setInterval(clock, 1000);
